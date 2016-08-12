f0a78fbf00c958b843b135406b229dce69dd0c04
tnga <devtnga@gmail.com>
2016-08-12 06:53:35 +0100

    (rollup) es6 to es5 sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/index.js',
+  sourceMap: true,
   moduleName: '_',
   format: 'umd',
   plugins: [babel() ],
--- underscore.js
+++ underscore.js
@@ -1865,3 +1865,4 @@
   return _;
 
 }));
+//# sourceMappingURL=underscore.js.map
