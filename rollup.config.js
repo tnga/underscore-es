43693ffb363fb5d7b02f5bd49632b156a1710f97
tnga <devtnga@gmail.com>
2016-08-12 11:18:44 +0100

    add banner information to the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'src/index.js',
-  sourceMap: true,
-  moduleName: '_',
-  format: 'umd',
-  plugins: [babel() ],
-  dest: 'underscore.js'
+	entry: 'src/index.js',
+	sourceMap: true,
+	moduleName: '_',
+	format: 'umd',
+	plugins: [babel()],
+	dest: 'underscore.js',
+	banner: '//     Underscore.js 1.8.3 \n' +
+					'//     http://underscorejs.org \n' +
+					'//     (c) 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors \n' +
+					'//     Underscore may be freely distributed under the MIT license.\n'
 };
--- underscore.js
+++ underscore.js
@@ -1,3 +1,8 @@
+//     Underscore.js 1.8.3 
+//     http://underscorejs.org 
+//     (c) 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors 
+//     Underscore may be freely distributed under the MIT license.
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
