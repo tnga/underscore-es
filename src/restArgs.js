0d8026447823647b24a5ebcacaa7a24cd3235866
tnga <devtnga@gmail.com>
2016-08-23 12:35:53 +0100

    function tools: provide `restArgs` individually

---
+++ src/restArgs.js
@@ -0,0 +1,5 @@
+// `_restArgs` : (ahem) a function's function
+// -------------------------------------------
+
+// This accumulates the arguments passed into an array, after a given index.
+export {restArgs as default} from './_internal';
