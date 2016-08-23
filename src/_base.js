1a1f33066ccf6c583ae81f23c8ad9cfeeffb088f
tnga <devtnga@gmail.com>
2016-08-23 10:17:58 +0100

    underscore's base constructor

---
+++ src/_base.js
@@ -0,0 +1,13 @@
+// `_` : base namespace and constructor for underscore's object
+// -------------------------------------------------------------
+
+// Baseline setup
+function _ (obj) {
+	if (obj instanceof _) return obj;
+	if (!(this instanceof _)) return new _(obj);
+	this._wrapped = obj;
+}
+// Current version.
+_.VERSION = '1.8.3';
+
+export {_ as default}; // @important: exportation of the function, not only it definition
