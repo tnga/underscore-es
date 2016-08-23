1d4c1d7e0ab67a417a599aa98def81a0f9617305
tnga <devtnga@gmail.com>
2016-08-23 11:12:01 +0100

    function tools: provide `before` individually

---
+++ src/before.js
@@ -0,0 +1,14 @@
+// `_before` : (ahem) a function's function
+// -----------------------------------------
+
+// Returns a function that will only be executed up to (but not including) the Nth call.
+export default function (times, func) {
+  var memo;
+  return function () {
+    if (--times > 0) {
+      memo = func.apply(this, arguments);
+    }
+    if (times <= 1) func = null;
+    return memo;
+  };
+}
