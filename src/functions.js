b399d44fba7a0b8d13cd9fc518c75a2d31b15db9
tnga <devtnga@gmail.com>
2016-08-23 12:09:47 +0100

    object tools: provide `functions` individually

---
+++ src/functions.js
@@ -0,0 +1,5 @@
+// `_functions` : an object's function
+// ------------------------------------
+
+// The cornerstone, a `functions` implementation, aka `methods`.
+export {default} from './methods';
