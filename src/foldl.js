df5ba65a6c9473f679865b51b1454e5261887028
tnga <devtnga@gmail.com>
2016-08-23 12:03:23 +0100

    collection tools: provide `foldl` individually

---
+++ src/foldl.js
@@ -0,0 +1,5 @@
+// `_foldl` : a collection's function
+// -----------------------------------
+
+// **Reduce** builds up a single result from a list of values, aka `inject`,
+export {default} from './inject';
