2f5d175b9b10c9f0aad19b6f58664ff742af289d
tnga <devtnga@gmail.com>
2016-08-23 12:42:27 +0100

    function tools: provide `throttle` individually

---
+++ src/throttle.js
@@ -0,0 +1,50 @@
+// `_throttle` : (ahem) a function's function
+// -------------------------------------------
+
+import _now from './now';
+
+// Returns a function, that, when invoked, will only be triggered at most once
+// during a given window of time. Normally, the throttled function will run
+// as much as it can, without ever going more than once per `wait` duration;
+// but if you'd like to disable the execution on the leading edge, pass
+// `{leading: false}`. To disable execution on the trailing edge, ditto.
+export default function (func, wait, options) {
+  let timeout, context, args, result;
+  let previous = 0;
+  if (!options) options = {};
+
+  let later = function () {
+    previous = options.leading === false ? 0 : _now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+
+  let throttled = function () {
+    let now = _now();
+    if (!previous && options.leading === false) previous = now;
+    let remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+
+  throttled.cancel = function () {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = context = args = null;
+  };
+
+  return throttled;
+}
