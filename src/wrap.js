fd2843713a1f0e2d8f69ec8e8802335d57a34923
tnga <devtnga@gmail.com>
2016-08-23 12:56:22 +0100

    function tools: provide `wrap` individually

---
+++ src/wrap.js
@@ -0,0 +1,11 @@
+// `_wrap` : (ahem) a function's function
+// ---------------------------------------
+
+import _partial from './partial';
+
+// Returns the first function passed as an argument to the second,
+// allowing you to adjust arguments, run code before and after, and
+// conditionally execute the original function.
+export default function (func, wrapper) {
+  return _partial(wrapper, func);
+}
