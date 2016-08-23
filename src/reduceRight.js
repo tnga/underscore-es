9518353aeb10ccae4246a74875eacd6dc96cc993
tnga <devtnga@gmail.com>
2016-08-23 12:34:51 +0100

    collection tools: provide `reduceRight` individually

---
+++ src/reduceRight.js
@@ -0,0 +1,5 @@
+// `_reduceRight` : a collection's function
+// ----------------------------------------
+
+// The right-associative version of reduce, also known as `foldr`
+export {default} from './foldr';
