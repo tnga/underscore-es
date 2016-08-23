fcf27dbb342abfc3e35864ba6ebd3cca8cf0bfc9
tnga <devtnga@gmail.com>
2016-08-23 11:11:34 +0100

    object tools: provide `assign` individually

---
+++ src/assign.js
@@ -0,0 +1,6 @@
+// `_assign` : an object's function
+// ------------------------------------
+
+// Assigns a given object with all the own properties in the passed-in object(s).
+// (https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object/assign)
+export {default} from './extendOwn' ;
