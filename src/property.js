8e99e3ca9813a962cec9422f04ef736e6167d45d
tnga <devtnga@gmail.com>
2016-08-23 11:42:45 +0100

    utility tools: provide `property` individually

---
+++ src/property.js
@@ -0,0 +1,5 @@
+// `_property` : an utility's function
+// ------------------------------------
+
+// function used for get key's value from an object.
+export {property as default} from './_internal';
