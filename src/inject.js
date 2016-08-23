c4966eb4b7a556636ca7a9f1b4715c3f1549b2e0
tnga <devtnga@gmail.com>
2016-08-23 12:02:55 +0100

    collection tools: provide `inject` individually

---
+++ src/inject.js
@@ -0,0 +1,9 @@
+// `_inject` : a collection's function
+// ------------------------------------
+
+import {createReduce} from './_internal';
+
+// **Reduce** builds up a single result from a list of values.
+var _inject = createReduce(1);
+
+export {_inject as default};
