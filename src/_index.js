3e769557a35411f13564636c364482696c4f457e
tnga <devtnga@gmail.com>
2016-08-26 09:13:43 +0100

    global index file for direct importation of all tools

---
+++ src/_index.js
@@ -0,0 +1,13 @@
+//     Underscore.js for ES6 and beyond usage !
+//     http://underscorejs.org
+//     (c) 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//		 (c) 2016 Tindo N. Arsel <devtnga@gmail.com>
+//     Underscore may be freely distributed under the MIT license.
+
+
+export * from './_collections';
+export * from './_arrays';
+export * from './_objects';
+export * from './_functions';
+export * from './_utilities';
+export {default} from './_namespace';
