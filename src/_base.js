7c32c53773331099c083c6930fd1094ed13b8ebe
tnga <devtnga@gmail.com>
2016-08-26 09:16:04 +0100

    bump to version 1.9.1

--- src/_base.js
+++ src/_base.js
@@ -8,6 +8,6 @@ function _ (obj) {
 	this._wrapped = obj;
 }
 // Current version.
-_.VERSION = '1.9.0';
+_.VERSION = '1.9.1';
 
 export {_ as default}; // @important: exportation of the function, not only it definition
